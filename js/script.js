c2038f409da45b2cd09f6c43317e84bb44694643
Martin Albert <albert@killkitsch.com>
2019-12-10 21:18:47 +0100

    script.js - deleted not used code

--- js/script.js
+++ js/script.js
@@ -32,52 +32,6 @@ linkedIn.innerHTML = "Linked In";
 const show_kontakt = document.createElement("div");
 show_kontakt.style.display = "none";
 
-let radic = 0;
-kontakt_button.addEventListener("click", event => {
-	
-	radic += 1;
-	event.preventDefault();
-
-	console.log("mail.innerHTML");
-
-	if (radic == 1) {
-		
-		style = {
-			width: "100%",
-			height: "100%",
-			display: "flex",
-			flexDirection: "column",
-			justifyContent: "center",
-			alignItems: "center",
-			padding: "50px 30px",
-			transition: "all 300ms ease-in-out"
-
-		}
-	} else {
-		style = {
-			display: "none"
-		};
-		radic -= 2;
-	}
-
-	show_kontakt.style.width = style.width;
-	show_kontakt.style.flexDirection = style.flexDirection;
-	show_kontakt.style.justifyContent = style.justifyContent;
-	show_kontakt.style.alignItems = style.alignItems;
-	show_kontakt.style.padding = style.padding;
-	
-	setTimeout(function() {
-		show_kontakt.style.display = style.display;
-	}, 300);
-
-	show_kontakt.appendChild(tel_cislo);
-	show_kontakt.appendChild(mail);
-	show_kontakt.appendChild(linkedIn);
-
-	kontakt_button.appendChild(show_kontakt);
-	console.log('end');
-
-});
 
 const brandOnTop = "assets/imgs/logo.png, assets/imgs/logo-2x.png 2x, assets/imgs/logo-3x.png 3x";
 const brandScrolling = "assets/imgs/logo2.png, assets/imgs/logo2-2x.png 2x, assets/imgs/logo2-3x.png 3x";
@@ -119,7 +73,7 @@ $(window).scroll(function() {
 		});
 	}
 	if ($('.navbar').offset().top >= $('#chemex').offset().top){
-		$('#methods-nav').show();
+		// $('#methods-nav').show();
 	} else {
 		$('#methods-nav').hide();
 	}
