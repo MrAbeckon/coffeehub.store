9db5687d5c0c86972615c3fca37aa8a8fd4483fe
Martin Albert <albert@killkitsch.com>
2019-12-15 16:57:10 +0100

    script complete

--- js/script.js
+++ js/script.js
@@ -1,81 +1,31 @@
-function disableScroll() { 
-    // Get the current page scroll position 
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
-  
-        // if any scroll is attempted, set this to the previous value 
-        window.onscroll = function() { 
-            window.scrollTo(scrollLeft, scrollTop); 
-        }; 
-};
-// disableScroll();
-  
-function enableScroll() { 
-    window.onscroll = function() {}; 
-}
-const kontakt_button = document.getElementById("kontakt");
-const padding = "15px";
-
-const tel_cislo = document.createElement("p");
-tel_cislo.innerHTML = "+421 902 611 679";
-tel_cislo.style.paddingBottom = padding;
-
-const mail = document.createElement("a");
-mail.href = "martin.albert.187@gmail.com";
-mail.innerHTML = "martin.albert.187@gmail.com";
-mail.style.paddingBottom = padding;
-
-const linkedIn = document.createElement("a");
-linkedIn.href = "https://linkedin.com/in/martin-albert-29005a16b/";
-linkedIn.innerHTML = "Linked In";
-
-const show_kontakt = document.createElement("div");
-show_kontakt.style.display = "none";
-
-
 const brandOnTop = "assets/imgs/logo.png, assets/imgs/logo-2x.png 2x, assets/imgs/logo-3x.png 3x";
 const brandScrolling = "assets/imgs/logo2.png, assets/imgs/logo2-2x.png 2x, assets/imgs/logo2-3x.png 3x";
 const videoPath = "assets/videos/";
 var trigger = false;
+var hash = "#chemex";
 
-$('#scroll-down').click(function() {
-	enableScroll();
-	console.log('test')
-});
+// navigation bar dropdowns
+$('.dropdown').hover(() => {
+	$('.dropdown-menu').slideDown( "fast" );
+}, () => {
+	$('.dropdown-menu').slideUp("slow");
+})
+
+// navigation bar brand changing
 if ($(".navbar").offset().top > 50) {
 	$('.navbar').addClass('affix');
 	$('.navbar-brand img').attr('src','assets/imgs/logo2.png');
 	$('.navbar-brand img').attr('srcset', brandScrolling);
 }
-var hash = "#chemex";
-$('a').each(function(){
-	$(this).click(function(){
-		enableScroll();
-		hash = $(this)[0].hash;
-	})
-});
-$('.method-item-content-links-button-2').click(function() {
-	console.log($(this).children('a')[0].hash)
 
-	window.location.replace($(this).children('a')[0].hash);
+// redirect after click on button 
+$('.button-img').click(function() {	
+	window.location.replace('eshop.html');
 })
 
 $(window).scroll(function() {
 	if ($('.navbar').offset().top > 150) {
 
-		// modify this so it can have a smooth transition between videos
-		// $(window).one('scroll', function() {
-			
-		// 	if ($('.navbar').offset().top > 150) {
-		// 		if (!trigger)
-		// 			$('#main-video').attr('src', videoPath+"brewing_drops.mp4")
-		// 		trigger = true;
-		// 	} else {
-		// 		if (trigger)
-		// 			$('#main-video').attr('src', videoPath+"brewing_filter.mp4")
-		// 		trigger = false;
-		// 	}
-		// });
 	}
 	if ($('.navbar').offset().top >= $('#chemex').offset().top){
 		// $('#methods-nav').show();
@@ -106,3 +56,152 @@ $(window).scroll(function() {
 		// disableScroll();
 	}
 });
+
+// CAROUSEL logic
+if (document.querySelector(".button-right") != null && 
+	document.querySelector(".button-left") != null) {
+	let slides = document.querySelectorAll(".mycarousel .mycarousel-item");
+	let currentSlide = 0;
+	const nextButton = document.querySelector(".button-right");
+	const prevButton = document.querySelector(".button-left");
+
+	function nextSlide() {
+		slides[currentSlide].className = 'mycarousel-item';
+		currentSlide = (currentSlide + 1) % slides.length;
+		slides[currentSlide].className = 'mycarousel-item show';
+	}
+
+	function prevSlide() {
+		slides[currentSlide].className = 'mycarousel-item';
+		currentSlide = (currentSlide - 1) % slides.length;
+		if (currentSlide < 0)
+			currentSlide = slides.length - 1;
+		slides[currentSlide].className = 'mycarousel-item show';
+	}
+
+	nextButton.addEventListener("click", () => {
+		nextSlide();
+	});
+	prevButton.addEventListener("click", () => {
+		prevSlide();
+	});
+
+
+	function positionSliderButton() {
+		let slider = document.querySelector('.mycarousel');
+		let sliderHeight = slider.getBoundingClientRect().height;
+		let buttons = document.querySelectorAll('.slider-button');
+
+		for (button of buttons) {
+			let buttonHeight = button.getBoundingClientRect().height;
+			button.style.top = (((sliderHeight - buttonHeight) / 2).toString()) + "px";
+		}
+	}
+	positionSliderButton();
+
+	window.addEventListener('resize', () => {
+		positionSliderButton();
+	});
+}
+
+// DYNAMIC PHOTO GALLERY
+if (document.title == "Chemex | Coffeehub.store"){
+	const productImgs = $('.product-image');
+	const gallery = $('<div></div>').addClass('gallery');
+	const galleryImgs = productImgs;
+	let controls = $('<div></div>').addClass('controls');
+	let openGallery = false;
+
+	productImgs.click(function(){
+		$('body').append(gallery);
+		openGallery = true;
+
+		let imgPath = $(this).attr('src');
+		let galleryDiv = $('.gallery');
+		let currImg = $('<img>').attr('src',imgPath).addClass('current-image');
+		galleryDiv.append(currImg);
+		galleryDiv.append(controls);
+		$('.controls').append("<img class='prev-img' src='assets/icons/arrow-down-2x.png'>");
+		$('.controls').append("<img class='next-img' src='assets/icons/arrow-down-2x.png'>");
+
+		$('.gallery').click(function(event) {
+			
+			if($('.current-image').length && openGallery){
+				if (!$(event.target).parent('div').hasClass('controls') && !$(event.target).hasClass('controls')) {
+					if(!$(event.target).closest('.current-image').length) {
+						$('.controls').empty();
+						$('.gallery').empty().remove();
+					}
+				}
+			}
+		});
+
+		$('.prev-img').click(function(){
+
+			let curSrc = $('.current-image')[0].src;
+			let iter = 0;
+			if($('.current-image').length && openGallery){
+				
+				galleryImgs.each(function() {
+					let prevSrc = $(this)[0].src;
+					// found the current index
+					if (curSrc == prevSrc) {
+						return false;
+					}
+					iter++;
+				});	
+				let i = (iter - 1) % galleryImgs.length;
+				if (i < 0)
+					i = galleryImgs.length -1;
+
+				$('.current-image')[0].src = galleryImgs[i].src
+			}
+		})
+		$('.next-img').click(function(){
+			let curSrc = $('.current-image')[0].src;
+			let iter = 0;
+			if($('.current-image').length && openGallery){
+				
+				galleryImgs.each(function() {
+					let prevSrc = $(this)[0].src;
+					// found the current index
+					if (curSrc == prevSrc) {
+						return false;
+					}
+					iter++;
+				});	
+				let i = (iter + 1) % galleryImgs.length;
+				if (i >= galleryImgs.length)
+					i = 0;
+
+				$('.current-image')[0].src = galleryImgs[i].src
+			}
+		})
+	});
+}
+
+// COUNTER
+if (document.title == "Eshop | Coffeehub.store"){
+
+	const completeDate = new Date('January 1, 2020 00:00:00').getTime();
+	const counter = $('#counter').children('h1');
+
+	var x = setInterval(function() {
+	  
+		var today = new Date().getTime();
+		var diff = completeDate - today;
+	  
+		var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+		console.log(days, hours, minutes, seconds);
+		counter.text(days + "d " + hours + "h "+ minutes + "m " + seconds + "s ");
+	  
+		// finished
+		if (diff < 0) {
+		  clearInterval(x);
+		  document.getElementById("demo").innerHTML = "EXPIRED";
+		}
+	  }, 1000);
+}
